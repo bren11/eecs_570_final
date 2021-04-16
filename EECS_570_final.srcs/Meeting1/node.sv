b4da87b51570afbf264d0f73ad047c6f7494aee5
John Straetmans <jstraet@umich.edu>
2021-04-16 17:26:06 -0400

    Some changes made to node.  Need to work on timing to make sure there aren't gap cycles (ships passing in the night)

--- EECS_570_final.srcs/Meeting1/node.sv
+++ EECS_570_final.srcs/Meeting1/node.sv
@@ -6,25 +6,19 @@ module NODE ( input             clk,
               input             router_full,
             
               output done,
-              output full,
+              //output full, don't need full since operation takes a single cycle
               output ACTIVATION_VALUE output_comb
             );
 
     parameter [`LAYER_BITS-1:0] node_id = 0;
     parameter [`NUM_BITS-1:0] layer_id = 0;
 
-    BUS_PACKET[`INPUT_BUFFER_SIZE-1:0] input_buffer;
-    BUS_PACKET[`INPUT_BUFFER_SIZE-1:0] n_input_buffer;
+    BUS_PACKET input_reg;
+    BUS_PACKET input_reg_n;
+    logic input_reg_valid;
 
     logic [`LAYER_SIZE-1:0] completed_inputs;   // which inputs for this layer have been used
     logic [`LAYER_SIZE-1:0] n_completed_inputs;
-
-    logic [`INPUT_BUFFER_BITS-1:0] head;
-    logic [`INPUT_BUFFER_BITS-1:0] n_head;
-    logic [`INPUT_BUFFER_BITS-1:0] tail;
-    logic [`INPUT_BUFFER_BITS-1:0] n_tail;
-
-    logic empty;
 
     ACTIVATION_VALUE weight;
     ACTIVATION_VALUE act;
@@ -37,9 +31,6 @@ module NODE ( input             clk,
     logic [`LAYER_BITS-1:0] num_connections_n;
 
     CONFIG cfg;
-    
-    assign full = (head == tail) & input_buffer[head].valid;
-    assign empty = (head == tail) & ~input_buffer[head].valid;
 
     assign done = (completed_inputs == cfg.connection_mask) & |(cfg.connection_mask);
 
@@ -59,62 +50,46 @@ module NODE ( input             clk,
         partial_output = 0;
         output_register_n = output_comb;
 
-        n_input_buffer = input_buffer;
-        n_head = head;
-        n_tail = tail;
+        input_reg = input_reg_n;
         n_completed_inputs = completed_inputs;
 
         /////////////// check connection mask ///////////////
         if (bus_in.valid & cfg.connection_mask[bus_in.neuron_num]) begin
             /////////////// receive input ///////////////
-            if (~full) begin
-                n_input_buffer[tail] = bus_in;
-                if (tail + 1 == `INPUT_BUFFER_SIZE)
-                    n_tail = 0;
-                else
-                    n_tail = tail + 1;
-            end
+            input_reg_n = bus;
         end
 
 
         /////////////// get weight and activation at the head of the buffer ///////////////
 
-        if (~done & ~empty) begin
-            weight = cfg.weights[input_buffer[head].neuron_num];
-            act = input_buffer[head].value;
+        if (~done & input_reg.valid) begin
+            weight = cfg.weights[input_reg.neuron_num];
             /////////////// operate ///////////////
             case (cfg.op_type)
 
                 MACC: begin
-                    partial_output = weight * act;
+                    partial_output = weight * input_reg;
                     output_register_n = output_register + partial_output;
                 end
 
                 MAXPOOL: begin
-                    if (output_register > act) begin
-                        output_register_n = act;
+                    if (output_register > input_reg) begin
+                        output_register_n = input_reg;
                     end
                 end
 
                 AVGPOOL: begin
-                    output_register_n = output_register + (act / num_connections);
+                    output_register_n = output_register + (input_reg / num_connections);
                 end
 
                 ADD: begin
-                    output_register_n = output_register + act;
+                    output_register_n = output_register + input_reg;
                 end
 
             endcase
         
             /////////////// update completed inputs ///////////////
-            n_completed_inputs[input_buffer[head].neuron_num] = 1'b1;
-
-            // increment head
-            if (head + 1 == `INPUT_BUFFER_SIZE) begin
-                n_head = 0;
-            end else begin
-                n_head = head + 1;
-            end
+            n_completed_inputs[input_reg.neuron_num] = 1'b1;
             
         end
 
